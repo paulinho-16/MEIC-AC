5701d835593f8c6be1ce3a797da03f587944d8a6
Diana Freitas <up201806230@fe.up.pt>
2021-11-16 11:56:39 +0000

    Use utils path function in loan exploration

--- src/data_understanding/loan_dataset.py
+++ src/data_understanding/loan_dataset.py
@@ -21,40 +21,41 @@ def loan_train_du():
 
 def loan_train_distribution(df):  
     sns.histplot(df['granted_date'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_date.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_date.jpg')
     plt.clf()
     df['granted_date'] = np.log(df['granted_date']) # log transformation
     sns.histplot(df['granted_date'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_date_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_date_log.jpg')
     plt.clf()
 
     sns.histplot(df['amount'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_amount.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_amount.jpg')
     plt.clf()
     df['amount'] = np.log(df['amount']) # log transformation
     sns.histplot(df['amount'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_amount_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_amount_log.jpg')
     plt.clf()
 
     sns.histplot(df['duration'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_duration.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_duration.jpg')
     plt.clf()
     df['duration'] = np.log(df['duration']) # log transformation
     sns.histplot(df['duration'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_duration_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_duration_log.jpg')
     plt.clf()
 
     sns.histplot(df['payments'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_payments_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_payments_log.jpg')
     plt.clf()
     df['payments'] = np.log(df['payments']) # log transformation
     sns.histplot(df['payments'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_payments_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_payments_log.jpg')
     plt.clf()
 
     sns.countplot(x ='loan_status', data = df)
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_status.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_status.jpg')
     plt.clf()
+
 
 def loan_test_du():
     df = db.df_query('SELECT * FROM loan_test')
@@ -63,35 +64,35 @@ def loan_test_du():
 
 def loan_test_distribution(df):
     sns.histplot(df['granted_date'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_date.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_date.jpg')
     plt.clf()
     df['granted_date'] = np.log(df['granted_date']) # log transformation
     sns.histplot(df['granted_date'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_date_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_date_log.jpg')
     plt.clf()
 
     sns.histplot(df['amount'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_amount.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_amount.jpg')
     plt.clf()
     df['amount'] = np.log(df['amount']) # log transformation
     sns.histplot(df['amount'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_amount_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_amount_log.jpg')
     plt.clf()
 
     sns.histplot(df['duration'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_duration.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_duration.jpg')
     plt.clf()
     df['duration'] = np.log(df['duration']) # log transformation
     sns.histplot(df['duration'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_duration_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_duration_log.jpg')
     plt.clf()
 
     sns.histplot(df['payments'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_payments.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_payments.jpg')
     plt.clf()
     df['payments'] = np.log(df['payments']) # log transformation
     sns.histplot(df['payments'])
-    plt.savefig('data_understanding/plots/distribution/loan/loan_test_payments_log.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_test_payments_log.jpg')
     plt.clf()
 
 def loan_train_correlation(df):
@@ -105,7 +106,7 @@ def loan_train_correlation(df):
             annot=True, 
             linewidths=0.1,vmax=1.0, linecolor='white',
             annot_kws={'fontsize':12 })
-    plt.savefig('data_understanding/plots/correlation/loan/loan_train_correlation.jpg')
+    plt.savefig(get_correlation_folder('loan')/'loan_train_correlation.jpg')
     plt.clf()
 
 def loan_amount_status(df):
@@ -123,12 +124,11 @@ def loan_amount_status(df):
     ax2.set_title('Loan Amount')
     ax1.legend()
     ax2.legend()
-    plt.savefig('data_understanding/plots/distribution/loan/loan_train_amount_status.jpg')
+    plt.savefig(get_distribution_folder('loan')/'loan_train_amount_status.jpg')
     plt.clf()
 
 if __name__ == '__main__':
-    Path("data_understanding/plots/distribution/loan").mkdir(parents=True, exist_ok=True)
-    Path("data_understanding/plots/correlation/loan").mkdir(parents=True, exist_ok=True)
+    create_plots_folders('loan')
     print("### LOAN TRAIN ###")
     loan_train_du()
     print()
--- src/data_understanding/utils/utils.py
+++ src/data_understanding/utils/utils.py
@@ -22,8 +22,8 @@ def create_plots_folders(table):
     get_distribution_folder('card').mkdir(parents=True, exist_ok=True)
     get_correlation_folder('card').mkdir(parents=True, exist_ok=True)
 
-def get_distribution_folder(table):
+def get_correlation_folder(table):
     return Path('data_understanding/plots/correlation/')/table
 
-def get_correlation_folder(table):
+def get_distribution_folder(table):
     return Path('data_understanding/plots/distribution/')/table
