13c59fefc2aa06bf6f9326502ef3789cc96539da
Diana Freitas <up201806230@fe.up.pt>
2021-11-26 11:45:38 +0000

    Fix Makefile; Merge districts

--- src/clean.py
+++ src/clean.py
@@ -9,6 +9,8 @@ sys.path.insert(1, '.')
 from database import database
 db = database.Database('bank_database')
 
+#pd.set_option('display.max_columns', None)
+
 #######
 # Clean
 #######
@@ -31,18 +33,17 @@ def clean_districts(db):
     df.dropna(inplace=True) # Remove NaN values
 
     # Average Crimes
-    df["avg_crimes"] = (df["nr_commited_crimes_95"] + df["nr_commited_crimes_96"]) / 2.0
+    df["avg_crimes"] = df[['nr_commited_crimes_95', 'nr_commited_crimes_96']].mean(axis=1)
+    df.drop(columns=['nr_commited_crimes_95'], inplace=True)
+    df.drop(columns=['nr_commited_crimes_96'], inplace=True)
 
     # Average Unemployment
-    df["avg_unemployment"] = (df["unemployment_rate_95"] + df["unemployment_rate_96"]) / 2.0
+    df["avg_unemployment"] = df[['unemployment_rate_95', 'unemployment_rate_96']].mean(axis=1)
+    df.drop(columns=['unemployment_rate_95'], inplace=True)
+    df.drop(columns=['unemployment_rate_96'], inplace=True)
 
-    # TODO - decide if we should drop the non aggregated columns (unemployment and crimes 95,96)
-    # df.drop(columns=['nr_commited_crimes_95'], inplace=True)
-    # df.drop(columns=['nr_commited_crimes_96'], inplace=True)
-    # df.drop(columns=['unemployment_rate_95'], inplace=True)
-    # df.drop(columns=['unemployment_rate_96'], inplace=True)
-
-    # TODO - decide if the region is relevant, given that the id already specifies different regions
+    df = df[['district_id', 'avg_crimes', 'avg_unemployment','nr_inhabitants', 
+    'average_salary', 'nr_enterpreneurs_1000_inhabitants']]
 
     return df
 
@@ -72,6 +73,8 @@ def clean_clients(db):
 
     # TODO - decide if we should drop the birth_number now that we have age and birth_date
     df.drop(columns=['birth_number'], inplace=True)
+
+    df.rename(columns={'district_id': 'client_district_id'}, inplace=True)
 
     return df
 
@@ -110,6 +113,8 @@ def clean_account(db):
     # Format creation date
     df['creation_date'] = df['creation_date'].apply(lambda date: int('19' + str(date)))
     
+    df.rename(columns={'district_id': 'account_district_id'}, inplace=True)
+
     return df
 
 
@@ -123,13 +128,31 @@ def transform_status(df):
     df["loan_status"].replace({-1: 1}, inplace=True)
 
 def transform_numeric_categorical(df):
-    num_cols = ['amount', 'duration', 'age', 'payments','avg_amount','avg_balance','num_trans','days_between']
-    cat_cols = ['frequency', 'gender']
+    num_cols = ['amount', 'duration', 'payments', 'avg_amount','avg_balance','num_trans','days_between', 'age',
+        'avg_crimes', 'avg_unemployment', 'nr_inhabitants', 'average_salary', 'nr_enterpreneurs_1000_inhabitants']
+    cat_cols = ['frequency', 'gender', 'same_district'] # TODO: card type
+
+    #scaler = MinMaxScaler()
+    #df[num_cols] = scaler.fit_transform(df[num_cols])
+
+    # col_trans = ColumnTransformer([
+    #     ('num', MinMaxScaler(), num_cols),
+    #     ('cat', OneHotEncoder(drop='if_binary'), cat_cols)
+    # ])
+    # df_transformed = col_trans.fit_transform(df)
 
     scaler = MinMaxScaler()
-    df[num_cols] = scaler.fit_transform(df[num_cols])
+    scaler.fit(df[num_cols])
+    df[num_cols] = pd.DataFrame(scaler.transform(df[num_cols]), index=df[num_cols].index, columns=df[num_cols].columns)
 
+    # df.frequency = df.frequency.astype('category')
+    # df.frequency = df.frequency.astype('category') # TODO: ver vídeo do YT da conversão das categorical
+    # df.frequency = df.frequency.astype('category')
+
+    # TODO - verify if this is the best way 
     df = pd.get_dummies(df, prefix=cat_cols, columns = cat_cols, drop_first=True)
+
+    print(df)
 
     return df
 
@@ -140,7 +163,6 @@ def transform_numeric_categorical(df):
 
 # Submission 1 - only loan
 def clean1(output_name):
-    db = database.Database('bank_database')
 
     loan_train = db.df_query('SELECT granted_date, amount, duration, payments, loan_status FROM loan_train')
     # Transform Status - 1 => 0 (loan granted) and -1 => 1 (loan not granted - aim of the analysis)
@@ -152,7 +174,6 @@ def clean1(output_name):
 
 # For submission 2
 def clean2(output_name):
-    db = database.Database('bank_database')
 
     ############
     # TRAIN DATA
@@ -168,38 +189,35 @@ def clean2(output_name):
     df = pd.merge(loan_train, account, on='account_id', how="left")
     df = pd.merge(df, disp,  on='account_id', how="left")
     df = pd.merge(df, client,  on='client_id', how="left")
+    # TODO - fazer 2 gráficos para mostrar pq que escolhemos os dados do district relativos à account e não ao client
+    df = pd.merge(df, district, left_on='account_district_id', right_on='district_id') # associate account's district
     df = pd.merge(df, transaction_train, how="left", on="account_id")
+    # TODO - merge CARD.
 
     # Days between loan and account creation
     df['days_between'] = df['granted_date'] - df['creation_date']
-
-    # TODO - decide if we should drop the dates
     df.drop(columns=['creation_date', 'granted_date'], inplace=True)
-    
-    # TODO - train data
-    # - merge CARD. 
-    # - Understand type of relation between type and dispostion
-    # - decide if we want to use both the district info of the account and of the owner 
-    # - decide if we want to use the same_district attribute(indicates if the owner and account have the same district)
-    # - merge DISTRICT
+
+    # Boolean value telling if the account was created on the owner district
+    df['same_district'] = df['account_district_id'] == df['client_district_id']
 
     # Drop Irrelevant IDs
-    # TODO - drop district attributes and card if needed 
-    df.drop(columns=["account_id", "loan_id", "disp_id", "client_id", "district_id_x", "district_id_y"], inplace=True)
+    # TODO - drop card_id
+    df.drop(columns=["account_id", "loan_id", "disp_id", "client_id", "account_district_id", "client_district_id"], inplace=True)
 
     # Transform Status - 1 => 0 (loan granted) and -1 => 1 (loan not granted - aim of the analysis)
-    transform_status(df)
+    # transform_status(df)
 
     # Scale numeric variables and encode categorical variables
     df = transform_numeric_categorical(df)
 
     df.to_csv('clean_data/' + output_name + '-train.csv', index=False)
 
+
     ############
     # TEST DATA
     ############
 
-    # TODO - clean test data and save to output file
     loan_test = clean_loans(db, True)
     transaction_test = clean_transactions(db, True)
     card_test = clean_card(db, True)    
@@ -207,25 +225,20 @@ def clean2(output_name):
     df_test = pd.merge(loan_test, account, how="left",on="account_id")
     df_test = pd.merge(df_test, disp, how="left",on="account_id")
     df_test = pd.merge(df_test, client, how="left",on="client_id")
+    df_test = pd.merge(df_test, district, left_on='account_district_id', right_on='district_id') # associate account's district
     df_test = pd.merge(df_test, transaction_test, how="left",on="account_id")
+    # TODO - merge CARD. 
 
     # Days between loan and account creation
     df_test['days_between'] = df_test['granted_date'] - df_test['creation_date']
-
-    # TODO - decide if we should drop the dates
     df_test.drop(columns=['creation_date', 'granted_date'], inplace=True)
 
-    # TODO - test data
-    # - merge CARD. 
-    # - Understand type of relation between type and dispostion
-    # - decide if we want to use both the district info of the account and of the owner 
-    # - decide if we want to use the same_district attribute(indicates if the owner and account have the same district)
-    # - merge DISTRICT
+    # Boolean value telling if the account was created on the owner district
+    df_test['same_district'] = df_test['account_district_id'] == df_test['client_district_id']
 
     # Drop Irrelevant IDs
-    # TODO - drop district attributes and card if needed 
-
-    df_test.drop(columns=["account_id", "disp_id", "client_id", "district_id_x", "district_id_y", "loan_status"], inplace=True)
+    # TODO - drop card_id
+    df_test.drop(columns=["account_id", "disp_id", "client_id", "account_district_id", "client_district_id", "loan_status"], inplace=True)
 
     # Scale numeric variables and encode categorical variables
     df_test = transform_numeric_categorical(df_test)
