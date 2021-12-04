1f86bacfe5d448953022a5aa82d1a6eb07b68d0c
Diana Freitas <up201806230@fe.up.pt>
2021-12-04 18:49:03 +0000

    Feature Selection and Utils files

--- src/test.py
+++ src/test.py
@@ -4,63 +4,49 @@ import pandas as pd
 from pathlib import Path
 import pickle
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from utils import *
 
 def test(classifier_name, submission_name):
     df = pd.read_csv('clean_data/' + submission_name + '-test.csv', delimiter=",", low_memory=False)
 
-    best_attributes = []
-    try:
-        output_file = open("./models/attributes.pkl", 'rb')
-        best_attributes = pickle.load(output_file)
-        output_file.close()
-    except Exception:            
-        print("Error loading best features list")
-        sys.exit(1)
-    print(best_attributes)
-
-    df = df[['loan_id'] + best_attributes]
+    # Select best features
+    best_features = get_best_features()
+    df = df[['loan_id'] + best_features]
     x_test = df.set_index('loan_id')
 
+    # Scaling
     scaler = MinMaxScaler()
-
     x_test = normalize_if_not_tree_based(x_test, classifier_name, scaler)
     print(x_test.head())
 
+    # Get trained model
     models_folder = Path("models/")
     filename = models_folder/(classifier_name + '-' + submission_name + '.sav')
     model = joblib.load(filename)
 
+    # Predict
     prediction = model.predict_proba(x_test)[::,1]
 
-    # Create the pandas DataFrame
+    # Store results
     df_result = pd.DataFrame()
-
     loan_ids = df['loan_id']
-
     df_result['Id'] = loan_ids
     df_result['Predicted'] = prediction
 
     df_result.to_csv('results/' + classifier_name + '-' +  submission_name + '.csv', sep=',', index=False)
 
 
-
-###########
-# Normalize
-###########
-
-def not_tree_based(classifier_name):
-    return classifier_name not in ['decision_tree','random_forest','xgboost','gradient_boosting']
-
-def normalize_if_not_tree_based(df, classifier_name, scaler):
-    if (not_tree_based(classifier_name)):
-        return normalize(df, scaler)
-    return df
-
-def normalize(df, scaler):
-    transformed = scaler.fit_transform(df)
-    df = pd.DataFrame(transformed, index=df.index, columns=df.columns)
-    return df
-
+def get_best_features():
+    best_features = []
+    try:
+        output_file = open("./models/features.pkl", 'rb')
+        best_features = pickle.load(output_file)
+        output_file.close()
+    except Exception:            
+        print("Error loading best features list")
+        sys.exit(1)
+    print(best_features)
+    return best_features
 
 if __name__ == "__main__":
     test(sys.argv[1], sys.argv[2])
--- src/train.py
+++ src/train.py
@@ -3,38 +3,75 @@ import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse.construct import rand
-from sklearn import metrics
+from pathlib import Path
 import seaborn as sb
-from seaborn.axisgrid import Grid
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score, confusion_matrix
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, KFold
 from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, BaggingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2, f_classif, mutual_info_classif, f_regression, mutual_info_regression
-from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-import pickle
-from pathlib import Path
 from imblearn.over_sampling import SMOTE
-from xgboost import XGBClassifier
-from sklearn import preprocessing
 from statistics import mean
+from utils import *
+from feature_selection import *
 
 DEBUG = False
-RS = 42
+CROSS_VALIDATION = True
 
-################################################
-# Cross-Validation comparison of the classifiers
-################################################
+#######
+# Train
+#######
+
+def train(classifier_name, submission_name):
+    df = pd.read_csv('clean_data/' + submission_name + '-train.csv', delimiter=",", low_memory=False)
+
+    # Scaling
+    scaler = MinMaxScaler()
+    df = normalize_if_not_tree_based(df, classifier_name, scaler)
+    #normalized_df = normalize(df, scaler)
+
+
+    # Define goal feature
+    X = df.drop(columns=['loan_status'])
+    y = df['loan_status']
+
+    #X_normalized = normalized_df.drop(columns=['loan_status'])
+    #y_normalized = normalized_df['loan_status']
+
+
+    # Apply SMOTE on imbalanced dataset
+    oversample = SMOTE(random_state=RS)
+    X, y = oversample.fit_resample(X, y)
+    #X_normalized, y_normalized = oversample.fit_resample(X_normalized, y_normalized)
+
+
+    # Feature Selection
+    X = filter_feature_selection(X, y)
+    #X_normalized, y_normalized = filter_feature_selection(X_normalized, y_normalized)
+
+
+    # Get Classifier
+    classifier = get_classifier_best(classifier_name)
+    
+    # Fit Classifier and Predict
+    num_splits = 5
+    kf = KFold(num_splits, random_state=RS, shuffle=True)
+
+    if CROSS_VALIDATION:
+        cross_validation(X, y, classifier, kf, num_splits)
+    else:
+        #compare_classifiers(kf, X, y, X_normalized, y_normalized)
+        no_cross_validation(X, y, classifier, kf)
+
+    # Save model
+    models_folder = Path("models/")
+    filename = models_folder/(classifier_name + '-' + submission_name + '.sav')
+    joblib.dump(classifier, filename)
+
+
+##################
+# Cross-Validation
+##################
 
 def no_cross_validation(X, y, classifier, kf):
     status_values = list(y.unique())
@@ -150,42 +187,9 @@ def compare_classifiers(kfold, X, y, X_normalized, y_normalized):
     print("XGBOOST: %0.2f accuracy with a standard deviation of %0.2f" % (xgboost_scores.mean(), xgboost_scores.std()))
     print("BAGGING: %0.2f accuracy with a standard deviation of %0.2f" % (bagging_scores.mean(), bagging_scores.std()))
 
-
-def train(classifier_name, submission_name):
-    df = pd.read_csv('clean_data/' + submission_name + '-train.csv', delimiter=",", low_memory=False)
-
-    scaler = MinMaxScaler()
-    df = normalize_if_not_tree_based(df, classifier_name, scaler)
-    #normalized_df = normalize(df, scaler)
-
-    X = df.drop(columns=['loan_status'])
-    y = df['loan_status']
-
-    #X_normalized = normalized_df.drop(columns=['loan_status'])
-    #y_normalized = normalized_df['loan_status']
-
-    oversample = SMOTE(random_state=RS)
-    X, y = oversample.fit_resample(X, y)
-    #X_normalized, y_normalized = oversample.fit_resample(X_normalized, y_normalized)
-
-    X, y = filter_feature_selection(X, y)
-    #X_normalized, y_normalized = filter_feature_selection(X_normalized, y_normalized)
-
-    classifier = get_classifier_best(classifier_name)
-    
-    num_splits = 5
-    kf = KFold(num_splits, random_state=RS, shuffle=True)
-
-    #compare_classifiers(kf, X, y, X_normalized, y_normalized)
-    cross_validation(X, y, classifier, kf, num_splits)
-
-    #no_cross_validation(X, y, classifier, kf)
-
-    models_folder = Path("models/")
-    filename = models_folder/(classifier_name + '-' + submission_name + '.sav')
-    joblib.dump(classifier, filename)
-
-# smote for grid search https://stackoverflow.com/questions/50245684/using-smote-with-gridsearchcv-in-scikit-learn
+##############
+# Grid Search
+#############
 def grid_search(classifier_name, submission_name):
 
     # Split dataset into training set and test set
@@ -212,203 +216,6 @@ def grid_search(classifier_name, submission_name):
 
     print('Best Parameters: ', grid_results.best_params_)
     print('Best Score: ', grid_results.best_score_)
-
-
-################################
-# Filter Based Feature Selection
-################################  
-
-def filter_feature_selection(X, y, k=18):
-    models_folder = Path("models/")
-
-    bestfeatures = SelectKBest(score_func=f_classif, k=k) # f_classif, f_regression
-    fit = bestfeatures.fit(X,y)
-    dfscores = pd.DataFrame(fit.scores_)
-    dfcolumns = pd.DataFrame(X.columns)
-    featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-    featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-    print(featureScores.nlargest(k, 'Score'))  #print 10 best features
-
-    print(featureScores.nlargest(k,'Score')['Specs'].values.tolist())
-
-    best_attributes = featureScores.nlargest(k,'Score')['Specs'].values.tolist()
-
-    pickle.dump(best_attributes, open(models_folder/'attributes.pkl', "wb"))
-
-    X = X[best_attributes]
-
-    return X, y
-
-# TODO - check if this is filter based
-def select_features_RFECV(X,y,classifier_name):
-    models_folder = Path("models/")
-    classifier=get_classifier(classifier_name)
-    
-    bestfeatures = RFECV(classifier,scoring='roc_auc') 
-    fit = bestfeatures.fit(X,y)
-    dfscores = pd.DataFrame(fit.ranking_ ) 
-    dfcolumns = pd.DataFrame(X.columns)
-    featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-    
-    featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-    print(featureScores.nlargest(10, 'Score'))  #print 10 best features
-    print(featureScores.nlargest(10,'Score')['Specs'].values.tolist())
-    best_attributes = featureScores.nlargest(10,'Score')['Specs'].values.tolist()
-    pickle.dump(best_attributes, open(models_folder/'attributes.pkl', "wb"))
-    X = X[best_attributes]
-
-    return X, y
-
-
-#################################
-# Wrapper Based Feature Selection
-#################################
-def sequential_selection(classifier, forward, X, y, k_features=12):
-    models_folder = Path("models/")
-    sfs = SFS(classifier,
-          k_features=k_features,
-          forward=forward,
-          floating=False,
-          scoring = 'roc_auc',
-          cv = 0)
-
-    sfs.fit(X, y)
-    print(list(sfs.k_feature_names_))
-    best_attributes = list(sfs.k_feature_names_)
-
-    pickle.dump(best_attributes, open(models_folder/'attributes.pkl', "wb"))
-
-    X = X[best_attributes]
-
-    return X, y
-
-
-def get_classifier(classifier):
-    if classifier == 'decision_tree':
-        return DecisionTreeClassifier(random_state=RS)
-    elif classifier == 'logistic_regression':
-        return LogisticRegression(random_state=RS, max_iter=300)
-    elif classifier == 'random_forest':
-        return RandomForestClassifier(random_state=RS)
-    elif classifier == 'gradient_boosting':
-        return GradientBoostingClassifier(random_state=RS)
-    elif classifier == 'svm':
-        return SVC(random_state=RS)
-    elif classifier == 'knn':
-        return KNeighborsClassifier(random_state=RS)
-    elif classifier == 'neural_network':
-        return MLPClassifier(random_state=RS) # TODO: random_state
-    elif classifier == 'xgboost':
-        return XGBClassifier()
-
-def get_grid_params(classifier):
-    if classifier == 'decision_tree':
-        return {'criterion': ['gini', 'entropy'],
-                'splitter': ['best', 'random'],
-                'max_depth': [3,5,7],
-                'min_samples_split': [1,2,3],
-                'min_samples_leaf': [1,2,3],
-                'min_weight_fraction_leaf': [0.0],
-                'max_features': [None, 'auto', 'sqrt', 'log2', 12],
-                'max_leaf_nodes': [None],
-                'min_impurity_decrease': [0.0],
-                'class_weight': [None],
-                'ccp_alpha': [0.0]}
-    elif classifier == 'logistic_regression':
-        return {'penalty': ['l2', 'none'],
-                'C': [0.01, 0.05, 0.1, 0.2, 0.5, 1.0],
-                'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'],
-                'class_weight': ["balanced", None]}
-
-    elif classifier == 'random_forest':
-        max_depth = [int(x) for x in range(2, 16, 4)]
-        max_depth.append(None)
-
-        return {'n_estimators': [int(x) for x in range(2, 14, 2)],
-                'max_features': ['auto', 'sqrt'],
-                'max_depth': max_depth,
-                'criterion': ['gini', 'entropy'],
-                'min_samples_split':  [2, 4, 6, 8],
-                'min_samples_leaf':  [1, 2, 4, 6],
-                'class_weight': ["balanced", "balanced_subsample", None]}
-    
-    elif classifier == 'gradient_boosting':
-        return {'n_estimators': [int(x) for x in range(2, 14, 2)],
-            'learning_rate': [0.1, 0.3, 0.5, 0.7],
-            'loss': ['deviance', 'exponential'],
-            'criterion': ['friedman_mse', 'squared_error'],
-            'min_samples_split':  [4, 6, 8],
-            'min_samples_leaf':  [2, 4, 6]}
-
-    elif classifier == 'svm':
-        return {'C': [1, 10], 
-          'gamma': [0.001, 0.01, 1,'scale','auto'],
-          'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
-          'degree':[1,2,3,4,5,6,7],
-          'coef0':[0.0, 0.1, 0.3, 0.5, 0.7],
-          'max_iter':[1,2,3,5,7,10],
-          'decision_function_shape':['ovo','ovr'],
-          'class_weight':[None, 'balanced', dict]}
-
-    elif classifier == 'knn':
-        return {'n_neighbors': [3, 5, 7, 9],
-          'weights': ['uniform', 'distance'],
-          'algorithm':['auto', 'ball_tree', 'kd_tree', 'brute'],
-          'leaf_size':[20, 30, 40],
-          'p':[1, 2, 3],
-          'metric':['minkowski'],
-          'metric_params':[None],
-          'n_jobs':[None, 5]}
-
-    elif classifier == 'neural_network':
-        return {'hidden_layer_sizes': [(3,5,8,13,21,34)],
-          'activation': ['identity', 'logistic', 'tanh', 'relu'],
-          'solver':['lbfgs', 'sgd', 'adam'],
-          'learning_rate':['constant', 'invscaling', 'adaptive'],
-          'nesterovs_momentum':[True, False],
-          'early_stopping':[False, True]}
-        
-    elif classifier == 'xgboost':
-        return {'booster': ['gbtree', 'gblinear', 'dart']}
-
-    
-def get_classifier_best(classifier):
-    if classifier == 'decision_tree':
-        # return DecisionTreeClassifier(criterion='entropy', splitter='random')
-        return DecisionTreeClassifier()
-    elif classifier == 'logistic_regression':
-        return LogisticRegression(C = 0.1, class_weight= 'balanced', penalty= 'l2', solver= 'saga', max_iter=200)
-    elif classifier == 'random_forest':
-        return RandomForestClassifier(class_weight= 'balanced_subsample', criterion= 'gini', max_depth= 30)
-    elif classifier == 'gradient_boosting':
-        return GradientBoostingClassifier(random_state=RS, criterion='friedman_mse', learning_rate=0.7, loss= 'exponential', min_samples_leaf= 6, min_samples_split= 4, n_estimators= 12)
-    elif classifier == 'svm':
-        return SVC(random_state=RS, C= 1, class_weight= 'balanced', coef0= 0.0, decision_function_shape= 'ovo', degree= 5, gamma= 'scale', kernel= 'poly', max_iter= 1000, probability=True)
-    elif classifier == 'knn':
-        return KNeighborsClassifier(n_neighbors=5, weights='distance', leaf_size=20, p=1)
-    elif classifier == 'neural_network':
-        return MLPClassifier(activation='tanh', hidden_layer_sizes= (3, 5, 8, 13, 21, 34), solver='lbfgs', max_iter=300)
-    elif classifier == 'xgboost':
-        return XGBClassifier(colsample_bytree=0.8, gamma= 1.5, max_depth= 30, min_child_weight= 1, subsample= 1.0,)
-    elif classifier == 'bagging':
-        return BaggingClassifier(bootstrap_features= True, max_features= 1.0, max_samples= 1.0, n_estimators= 20)
-
-###########
-# Normalize
-###########
-
-def not_tree_based(classifier_name):
-    return classifier_name not in ['decision_tree','random_forest','xgboost','gradient_boosting']
-
-def normalize_if_not_tree_based(df, classifier_name, scaler):
-    if (not_tree_based(classifier_name)):
-        return normalize(df,scaler)
-    return df
-
-def normalize(df, scaler):
-    transformed = scaler.fit_transform(df)
-    df = pd.DataFrame(transformed, index=df.index, columns=df.columns)
-    return df
 
 
 if __name__ == "__main__":
